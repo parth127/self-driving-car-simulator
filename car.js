397467d2d6b8903ba42bd9d5db53743939b97676
Parth Vasoya <31893304+parth127@users.noreply.github.com>
2022-05-18 12:38:23 -0400

    Basic Car Mechanics

--- car.js
+++ car.js
@@ -6,32 +6,70 @@ class Car {
     this.width = width;
     this.height = height;
 
+    this.speed = 0;
+    this.acceleration = 0.5;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+
+    this.angle = 0;
+
     this.controls = new Controls();
   }
 
   update() {
-    if (this.controls.left) {
-      this.positionX -= 2;
-    }
-    if (this.controls.right) {
-      this.positionX += 2;
-    }
+    this.#move();
+  }
+
+  #move() {
     if (this.controls.forward) {
-      this.positionY -= 2;
+      this.speed += this.acceleration;
     }
     if (this.controls.reverse) {
-      this.positionY += 2;
+      this.speed -= this.acceleration;
     }
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+
+    this.positionX -= Math.sin(this.angle) * this.speed;
+    this.positionY -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.positionX, this.positionY);
+    ctx.rotate(-this.angle);
+
     ctx.beginPath();
-    ctx.rect(
-      this.positionX - this.width / 2,
-      this.positionY - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+
+    ctx.restore();
   }
 }
