96aac1c006f058f4cec71ad7d377a0958e244a9f
Parth Vasoya <31893304+parth127@users.noreply.github.com>
2022-05-19 13:20:42 -0400

    Implementation of Parallelization

--- car.js
+++ car.js
@@ -129,7 +129,7 @@ class Car {
     this.positionY -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor = false) {
     if (this.damaged) {
       ctx.fillStyle = "black";
     } else {
@@ -145,7 +145,7 @@ class Car {
 
     ctx.fill();
 
-    if (this.sensor) {
+    if (this.sensor && drawSensor) {
       this.sensor.draw(ctx);
     }
   }
--- index.js
+++ index.js
@@ -13,40 +13,84 @@ const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.925);
 
 //create a car object
-const car = new Car(road.getLaneCenter(1), 150, 30, 50, "AI");
-car.draw(carCtx);
+//const car = new Car(road.getLaneCenter(1), 150, 30, 50, "AI");
+const N = 100;
+const cars = generateCars(N);
+
+let bestCar = cars[0];
+if (localStorage.getItem("bestBrain")) {
+  bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
+}
 
 // create traffic for the road
 
-const traffic = [new Car(road.getLaneCenter(1), -150, 30, 50, "DUMMY", 2)];
+const traffic = [
+  new Car(road.getLaneCenter(1), -150, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -350, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+];
 
 //animate the car
 
 animate();
+
+//function to save the best car in localStorage
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+//function to remove the best car from localStorage
+function discard() {
+  localStorage.removeItem("bestBrain");
+}
+
+//function to generate N number of cars to train the model
+function generateCars(N) {
+  const cars = [];
+  for (let i = 0; i < N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), 150, 30, 50, "AI"));
+  }
+
+  return cars;
+}
 
 function animate(time) {
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
   }
 
-  car.update(road.borders, traffic);
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].update(road.borders, traffic);
+  }
+
+  //Code to find the best car
+  bestCar = cars.find(
+    (c) => c.positionY === Math.min(...cars.map((c) => c.positionY))
+  );
+
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
 
   carCtx.save();
-  carCtx.translate(0, -car.positionY + carCanvas.height * 0.7);
+  carCtx.translate(0, -bestCar.positionY + carCanvas.height * 0.7);
 
   road.draw(carCtx);
 
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].draw(carCtx, "red");
   }
-  car.draw(carCtx, "blue");
+
+  carCtx.globalAlpha = 0.2;
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].draw(carCtx, "blue");
+  }
+  carCtx.globalAlpha = 1;
+  bestCar.draw(carCtx, "blue", true);
 
   carCtx.restore();
 
   networkCtx.lineDashOffset = -time / 50;
-  Visualizer.drawNetwork(networkCtx, car.brain);
+  Visualizer.drawNetwork(networkCtx, bestCar.brain);
 
   requestAnimationFrame(animate);
 }
